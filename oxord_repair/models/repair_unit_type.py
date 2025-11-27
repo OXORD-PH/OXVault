2eec1fbfb4a589f19c0d297bda8b0cf91bdf945f
Jimmy OXORD <oxord@example.com>
2025-11-27 17:19:16 +0800

    Remove oxord_repair module from Production

--- oxord_repair/models/repair_unit_type.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class RepairUnitType(models.Model):
-    _name = 'repair.unit.type'
-    _description = 'Repair Unit Type'
-
-    name = fields.Char(string="Unit Type", required=True)
-    department_id = fields.Many2one('hr.department', string="Department", required=True)
--- oxord_repair/models/res_partner.py
+++
@@ -1,6 +0,0 @@
-from odoo import fields, models
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    facebook = fields.Char(string="Facebook")
