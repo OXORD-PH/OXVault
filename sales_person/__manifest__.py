53ee5f98ee80c6edc622074717598ac23a11e47c
Jimmy OXORD <oxord@example.com>
2025-12-16 13:47:24 +0800

    Add sales_person module

--- sales_person/__manifest__.py
+++ sales_person/__manifest__.py
@@ -1,16 +1,16 @@
 {
-    'name': 'Sales Person On POS Order Line',
-    'Version': '18.0.1.0.0',
+    'name': 'POS Sales Person',
+    'Version': '19.0.1.0.0',
     'category': 'Extra Tools',
     'summary': 'This module is used to set sales persons on pos order line',
     'description': 'This module allows you to assign salespersons to order'
                    'lines in the Point of Sale (POS)',
-    'author': 'Cybrosys Techno solutions',
-    'company': 'Cybrosys Techno Solutions',
-    'maintainer': 'Cybrosys Techno Solutions',
-    'website': "https://www.cybrosys.com",
+    'author': 'OXORD Computer solutions',
+    'company': 'OXORD Computer solutions',
+    'maintainer': 'OXORD Computer solutions',
+    'website': "https://www.oxord.com.ph",
     'depends': ['point_of_sale'],
-	'assets': {
+    'assets': {
         'point_of_sale.assets': [
             'salesperson/static/src/js/salesperson_button.js',
             'salesperson/static/src/js/salesperson_popup.js',
@@ -18,5 +18,8 @@
             'salesperson/static/src/xml/salesperson_popup.xml',
         ],
     },
+    'license': 'AGPL-3',
     'installable': True,
+    'auto_install': False,
+    'application': False,
 }
