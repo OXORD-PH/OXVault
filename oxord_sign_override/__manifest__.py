5bc51d0031038d8e5142dda802210c05e59846e0
Jimmy OXORD <oxord@example.com>
2025-12-19 15:40:04 +0800

    Add OXORD Sign Override module

---
+++ oxord_sign_override/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'OXORD Sign Override',
+    'version': '1.0',
+    'author': 'OXORD Computer Solutions',
+    'category': 'Tools',
+    'summary': 'Override Sign module CSS to hide frame/footer',
+    'depends': ['sign'],
+    'data': [
+        'views/assets.xml',
+    ],
+    'installable': True,
+    'application': False
+}
