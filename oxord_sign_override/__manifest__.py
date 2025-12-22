96ea1a2a581fb98c62654235b4c13b3f4400aca1
OXORD-PH <system@oxord.com.ph>
2025-12-22 11:45:19 +0800

    Rename module to OXORD Sign Override3
    Updated module name and summary for clarity.

--- oxord_sign_override/__manifest__.py
+++ oxord_sign_override/__manifest__.py
@@ -1,15 +1,15 @@
 {
-    'name': 'OXORD Sign Override2',
-    'version': '1.0',
-    'author': 'OXORD Computer Solutions',
-    'category': 'Tools',
-    'summary': 'Override Sign module CSS to hide frame/footer',
-    'depends': ['sign'],
-    'assets': {
-        'web.assets_frontend': [
-            'oxord_sign_override/static/src/scss/sign_override.scss',
+    "name": "OXORD Sign Override3",
+    "version": "1.0",
+    "summary": "Hide sign frame styling in Odoo 19",
+    "author": "OXORD Computer Solutions & Repair Center",
+    "category": "Customization",
+    "depends": ["sign", "web"],
+    "assets": {
+        "web.assets_backend": [
+            "oxord_sign_override/static/src/scss/sign_override.scss",
         ],
     },
-    'installable': True,
-    'application': False
+    "installable": True,
+    "application": False,
 }
