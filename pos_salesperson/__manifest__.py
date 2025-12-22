0479a650e584f9cf0cee89b9a0e218b3b506d557
Jimmy OXORD <oxord@example.com>
2025-12-22 18:11:38 +0800

    Add/Update pos_salesperson module to Mystaging

---
+++ pos_salesperson/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'POS Salesperson Button',
+    'depends': ['point_of_sale'],
+    'assets': {
+        'point_of_sale.assets': [
+            'pos_salesperson/static/src/js/salesperson_loader.js',
+            'pos_salesperson/static/src/js/salesperson_button.js',
+        ],
+    },
+}
