2447d903ffca5b75f106e339e6e1d68ee6810067
OXORD-PH <system@oxord.com.ph>
2025-12-26 11:39:47 +0800

    Delete oxord_repair directory

--- oxord_repair/models/res_company.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ResCompany(models.Model):
-    _inherit = 'res.company'
-
-    background_image = fields.Binary("PDF Background Image")
-    code = fields.Char(string="Branch Code")
-
-
