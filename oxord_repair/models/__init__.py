18e99f897abead741780ab686df8abaadb2e9820
OXORD-PH <system@oxord.com.ph>
2026-01-03 09:43:07 +0800

    Add files via upload

---
+++ oxord_repair/models/__init__.py
@@ -0,0 +1,15 @@
+# oxord_repair/models/__init__.py
+
+from . import repair_order
+from . import technical_report
+from . import account_move
+from . import sale_order_inherit
+from . import repair_endorse_wizard
+from . import endorse_coordinator_wizard
+from . import repair_problem
+from . import repair_unit_type
+from . import brand_model
+from . import product_template_extension
+from . import res_company
+from . import res_partner
+from . import res_partner_category
